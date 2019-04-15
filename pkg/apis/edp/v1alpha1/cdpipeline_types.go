abf96e2bd075361f12c8c6c7934b07662c962bfe
Stanislav Kostenko <stanislav_kostenko@epam.com>
2019-04-15 15:09:42 +0300

    [EPMDEDP-2380] init new controller for CR CDPipeline
    Change-Id: I06205863b13462aaf422d872391452ece7fe3aa7

---
+++ pkg/apis/edp/v1alpha1/cdpipeline_types.go
@@ -0,0 +1,49 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CDPipelineSpec defines the desired state of CDPipeline
+// +k8s:openapi-gen=true
+type CDPipelineSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// CDPipelineStatus defines the observed state of CDPipeline
+// +k8s:openapi-gen=true
+type CDPipelineStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CDPipeline is the Schema for the cdpipelines API
+// +k8s:openapi-gen=true
+type CDPipeline struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CDPipelineSpec   `json:"spec,omitempty"`
+	Status CDPipelineStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CDPipelineList contains a list of CDPipeline
+type CDPipelineList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CDPipeline `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CDPipeline{}, &CDPipelineList{})
+}
