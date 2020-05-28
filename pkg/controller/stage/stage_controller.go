fa038aae79e81e5e530c37e611f9cc62714737a0
Viktor Semenov <Viktor_Semenov2@epam.com>
2020-05-28 16:00:09 +0300

    [EPMDEDP-5088] Modify cd stage controller to create jenkins job with job provision to trigger
    Change-Id: I18dd49bf9f98fa28a8bf71ae530f1559d8aaa1da

--- pkg/controller/stage/stage_controller.go
+++ pkg/controller/stage/stage_controller.go
@@ -2,27 +2,30 @@ package stage
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"reflect"
+
+	edpv1alpha1 "github.com/epmd-edp/cd-pipeline-operator/v2/pkg/apis/edp/v1alpha1"
 	"github.com/epmd-edp/cd-pipeline-operator/v2/pkg/controller/helper"
 	"github.com/epmd-edp/cd-pipeline-operator/v2/pkg/util/consts"
 	"github.com/epmd-edp/cd-pipeline-operator/v2/pkg/util/finalizer"
-	"github.com/pkg/errors"
-	"io/ioutil"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/event"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
-	"time"
-
-	edpv1alpha1 "github.com/epmd-edp/cd-pipeline-operator/v2/pkg/apis/edp/v1alpha1"
+	codebasev1alpha1 "github.com/epmd-edp/codebase-operator/v2/pkg/apis/edp/v1alpha1"
 	jenv1alpha1 "github.com/epmd-edp/jenkins-operator/v2/pkg/apis/v2/v1alpha1"
+	"github.com/pkg/errors"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"reflect"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
@@ -148,10 +151,7 @@ func (r *ReconcileStage) setFinishStatus(s *edpv1alpha1.Stage) error {
 func (r *ReconcileStage) createJenkinsJob(s edpv1alpha1.Stage) error {
 	log.V(2).Info("start creating JenkinsJob CR", "name", s.Name)
 
-	b, err := ioutil.ReadFile("/usr/local/bin/pipelines/cd-pipeline.tmpl")
-	if err != nil {
-		return err
-	}
+	jc, _ := r.createJenkinsJobConfig(s)
 
 	jj := &jenv1alpha1.JenkinsJob{
 		TypeMeta: metav1.TypeMeta{
@@ -166,8 +166,8 @@ func (r *ReconcileStage) createJenkinsJob(s edpv1alpha1.Stage) error {
 			StageName:     &s.Name,
 			JenkinsFolder: &s.Spec.CdPipeline,
 			Job: jenv1alpha1.Job{
-				Name:   s.Spec.Name,
-				Config: string(b),
+				Name:   fmt.Sprintf("job-provisions/job/cd/job/%v", s.Spec.JobProvisioning),
+				Config: string(jc),
 			},
 		},
 	}
@@ -176,6 +176,94 @@ func (r *ReconcileStage) createJenkinsJob(s edpv1alpha1.Stage) error {
 	}
 	log.Info("JenkinsJob has been created", "name", s.Name)
 	return nil
+}
+
+func (r ReconcileStage) createJenkinsJobConfig(s edpv1alpha1.Stage) ([]byte, error) {
+	var strStage string
+	for i, qg := range s.Spec.QualityGates {
+		if i >= 1 {
+			strStage = fmt.Sprintf("%v,", strStage)
+		}
+		strStage = fmt.Sprintf("%v{\"name\":\"%v\", \"step_name\":\"%v\"}", strStage, qg.QualityGateType, qg.StepName)
+	}
+	source := s.Spec.Source
+	jpm := map[string]string{
+		"PIPELINE_NAME":         s.Spec.CdPipeline,
+		"STAGE_NAME":            s.Name,
+		"QG_STAGES":             strStage,
+		"GIT_SERVER_CR_VERSION": "v2",
+		"SOURCE_TYPE":           source.Type,
+	}
+
+	if source.Type == "library" {
+		library, err := r.setLibraryParams(s)
+		if err == nil {
+			jpm["LIBRARY_URL"] = library["url"]
+			jpm["LIBRARY_BRANCH"] = library["branch"]
+			jpm["GIT_CREDENTIALS_ID"] = library["credentials"]
+			jpm["GIT_SERVER_CR_NAME"] = library["gitServerName"]
+		} else {
+			jpm["SOURCE_TYPE"] = "default"
+		}
+	}
+	jc, err := json.Marshal(jpm)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Can't marshal parameters %v into json string", jpm)
+	}
+	return jc, nil
+}
+
+func (r ReconcileStage) setLibraryParams(stage edpv1alpha1.Stage) (map[string]string, error) {
+	cb, err := r.getLibraryParams(stage.Spec.Source.Library.Name, stage.Namespace)
+	if err != nil {
+		log.Error(err, "couldn't retrieve parameters for pipeline's library, default source type will be used",
+			"Library name", stage.Spec.Source.Library.Name)
+		return nil, err
+	}
+	gs, err := r.getGitServerParams(cb.Spec.GitServer, stage.Namespace)
+	if err != nil {
+		log.Error(err, "couldn't retrieve parameters for git server, default source type will be used",
+			"Git server", cb.Spec.GitServer)
+		return nil, err
+	}
+	return map[string]string{
+		"url": fmt.Sprintf("ssh://%v@%v:%v%v", gs.Spec.GitUser, gs.Spec.GitHost, gs.Spec.SshPort,
+			getPathToRepository(string(cb.Spec.Strategy), stage.Spec.Source.Library.Name, cb.Spec.GitUrlPath)),
+		"credentials":   gs.Spec.NameSshKeySecret,
+		"branch":        stage.Spec.Source.Library.Branch,
+		"gitServerName": cb.Spec.GitServer,
+	}, nil
+}
+
+func (r ReconcileStage) getLibraryParams(name, ns string) (*codebasev1alpha1.Codebase, error) {
+	nsn := types.NamespacedName{
+		Namespace: ns,
+		Name:      name,
+	}
+	i := &codebasev1alpha1.Codebase{}
+	if err := r.client.Get(context.TODO(), nsn, i); err != nil {
+		return nil, err
+	}
+	return i, nil
+}
+
+func (r ReconcileStage) getGitServerParams(name string, ns string) (*codebasev1alpha1.GitServer, error) {
+	nsn := types.NamespacedName{
+		Namespace: ns,
+		Name:      name,
+	}
+	i := &codebasev1alpha1.GitServer{}
+	if err := r.client.Get(context.TODO(), nsn, i); err != nil {
+		return nil, err
+	}
+	return i, nil
+}
+
+func getPathToRepository(strategy, name string, url *string) string {
+	if strategy == "import" {
+		return *url
+	}
+	return "/" + name
 }
 
 func newTrue() *bool {
