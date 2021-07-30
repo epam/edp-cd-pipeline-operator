7c52e393d03d84b77b583699d74a37ac671efbf1
oleksandr_frolov <oleksandr_frolov@epam.com>
2021-07-30 14:46:22 +0300

    [EPMDEDP-6970]: Fix cd-pipeline CRD
    - Remove codebaseBranch field from cd-pipeline CRD
    - Add new field subresources status to cd-pipeline CRD
    - Align inputDockerStreams field in cd-pipeline struct with CRD
    
    Change-Id: I5226ca14ea392ec061ced7ce24e162b4e67f7087

--- pkg/apis/edp/v1alpha1/cdpipeline_types.go
+++ pkg/apis/edp/v1alpha1/cdpipeline_types.go
@@ -16,8 +16,7 @@ type CDPipelineSpec struct {
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 	Name                  string   `json:"name"`
 	DeploymentType        string   `json:"deploymentType"`
-	CodebaseBranch        []string `json:"codebase_branch"`
-	InputDockerStreams    []string `json:"input_docker_streams"`
+	InputDockerStreams    []string `json:"inputDockerStreams"`
 	ApplicationsToPromote []string `json:"applicationsToPromote"`
 }
 
