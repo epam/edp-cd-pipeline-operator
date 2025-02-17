59d79c232491bb3cf4227de02450c3a2ad7750ff
Zorian Motso <zorian_motso@epam.com>
2025-02-17 17:21:20 +0200

    feat: Improve namespace deletion handling (#114)
    In case if namespace doesn't exist we get error trying to delete it.
    We have an error because Capsule allows the operators
    to delete only the existing namespace.
    Add check if the namespace exists before removing it.

--- controllers/stage/chain/delete_namespace.go
+++ controllers/stage/chain/delete_namespace.go
@@ -8,6 +8,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	cdPipeApi "github.com/epam/edp-cd-pipeline-operator/v2/api/v1"
 )
@@ -19,19 +20,26 @@ type DeleteNamespace struct {
 func (h DeleteNamespace) ServeRequest(ctx context.Context, stage *cdPipeApi.Stage) error {
 	l := ctrl.LoggerFrom(ctx).WithValues("namespace", stage.Spec.Namespace)
 
-	ns := &corev1.Namespace{
-		ObjectMeta: metaV1.ObjectMeta{
-			Name: stage.Spec.Namespace,
-		},
-	}
-
-	if err := h.multiClusterClient.Delete(ctx, ns); err != nil {
+	if err := h.multiClusterClient.Get(ctx, client.ObjectKey{Name: stage.Spec.Namespace}, &corev1.Namespace{}); err != nil {
 		if apierrors.IsNotFound(err) {
 			l.Info("Namespace has already been deleted")
 
 			return nil
 		}
 
+		return fmt.Errorf("failed to get namespace: %w", err)
+	}
+
+	if err := client.IgnoreNotFound(
+		h.multiClusterClient.Delete(
+			ctx,
+			&corev1.Namespace{
+				ObjectMeta: metaV1.ObjectMeta{
+					Name: stage.Spec.Namespace,
+				},
+			},
+		),
+	); err != nil {
 		return fmt.Errorf("failed to delete namespace: %w", err)
 	}
 
